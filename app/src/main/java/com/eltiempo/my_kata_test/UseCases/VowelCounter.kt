3a4719c22f2e7852925b49255102397c653892af
Jairo Torres <>
2021-02-19 09:13:49 -0500

    add : do some refactor and run the test again to verify app don´t crash

--- app/src/main/java/com/eltiempo/my_kata_test/UseCases/VowelCounter.kt
+++ app/src/main/java/com/eltiempo/my_kata_test/UseCases/VowelCounter.kt
@@ -2,11 +2,13 @@ package com.eltiempo.my_kata_test.UseCases
 
 class VowelCounter {
 
+    private val MAX_NUM_CHARACTERS_ALLOWED = 20
+
     fun count(word : String) : Any{
 
         if (word.isEmpty())return 0
 
-        if (word.length > 10) {
+        if (word.length > MAX_NUM_CHARACTERS_ALLOWED) {
             return throw IndexOutOfBoundsException()
         }
         return 0
