d82c05b5caddab021bc2426ab66ffb7eb92484b5
Jairo Torres <>
2021-02-19 09:16:52 -0500

    add : next request, make sure successful scenario.

--- app/src/test/java/com/eltiempo/my_kata_test/UseCases/VowelCounterTest.kt
+++ app/src/test/java/com/eltiempo/my_kata_test/UseCases/VowelCounterTest.kt
@@ -19,7 +19,7 @@ class VowelCounterTest {
     }
 
     @Test
-    fun `throw an error when string is greater than 10 characters`() {
+    fun `throw an error when string is greater than 20 characters`() {
 
         //given
         val word = "this string has a lot of characters"
@@ -32,6 +32,20 @@ class VowelCounterTest {
         } catch (e: IndexOutOfBoundsException) {
             //if execution reaches here, it indicates this exception was occured. so we need not handle it.
         }
+    }
+
+    @Test
+
+    fun success_scenario(){
+
+        //given
+        val word = "It has 4 vowels"
+        //when
+        val vC = VowelCounter()
+        val numberOfVowel = vC.count(word)
+        //then
+        assertEquals(numberOfVowel, 4)
+
     }
 
 }
