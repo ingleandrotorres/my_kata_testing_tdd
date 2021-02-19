459c6531253df1fd9db4a313cbfc3cf78e5066e7
Jairo Torres <>
2021-02-19 10:29:56 -0500

    add: apply plugin to import easier ids in views

--- app/src/main/java/com/eltiempo/my_kata_test/Activitys/MainActivity.kt
+++ app/src/main/java/com/eltiempo/my_kata_test/Activitys/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.eltiempo.my_kata_test.Activitys
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import com.eltiempo.my_kata_test.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+
+
+        //vowelEditText
+        vowelButton.setOnClickListener {
+
+        }
+        return super.onCreateView(name, context, attrs)
     }
 }
